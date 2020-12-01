064c6b92925e2530b47b15ae656fac14231a3c75
Jan Forman <info@janforman.com>
2020-12-01 14:45:08 +0000

    Revert "hidl: Build libhidltransport/libhwbinder"
    This reverts commit 4989453b3ef21e9f02b97527fe04ae96a1f96951.

--- device-common.mk
+++ device-common.mk
@@ -264,12 +264,7 @@ PRODUCT_PACKAGES += \
 
 # Dummy hidl lib for oreo blobs
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor \
-    libhidltransport \
-    libhidltransport.vendor \
-    libhwbinder \
-    libhwbinder.vendor
+    android.hidl.base@1.0
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
